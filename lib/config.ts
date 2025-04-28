51687dea101c269c39e06f748f4845f9d235ab41
Neeraj Pal <neerajap.a01@gmail.com>
2025-04-28 13:40:35 +0530

    Update config.ts

--- lib/config.ts
+++ lib/config.ts
@@ -2,7 +2,7 @@ import z from 'zod';
 
 const envSchema = z.object({
   APP_ENV: z.enum(['local', 'production']).default('local'),
-  API_ENDPOINT: z.string().trim().min(1).default('http://localhost:3000'),
+  API_ENDPOINT: z.string().trim() //.min(1).default('http://localhost:3000'),
 });
-
+// API_ENDPOINT: z.string().trim().min(1).default('http://localhost:3000')
 export const env = envSchema.parse(process.env);
